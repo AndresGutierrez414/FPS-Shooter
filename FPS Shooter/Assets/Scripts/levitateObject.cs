dcf7836570898a0b48a2a18e53445da4e6853429
AndresGutierrez414 <andresgutierrez414@outlook.com>
2023-04-07 20:50:49 -0500

    added to levitateObject script
    whatever the script is attached to will be able to levitate and spin on y direction

--- FPS Shooter/Assets/Scripts/levitateObject.cs
+++ FPS Shooter/Assets/Scripts/levitateObject.cs
@@ -4,15 +4,30 @@ using UnityEngine;
 
 public class levitateObject : MonoBehaviour
 {
+    // variables //
+    [SerializeField] float speed;
+    [SerializeField] float verticalSpeed;
+    [SerializeField] float height;
+
+    private Vector3 initialPosition;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        initialPosition = transform.position;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        // rotation //
+        transform.Rotate(new Vector3(0, 45, 0) * Time.deltaTime);
+
+        // up and down movement //
+        // Mathf.Sin -> creates sin wave that moves between -1 and 1
+        float yPos = initialPosition.y + Mathf.Sin(Time.time * verticalSpeed) * height;
+        // calculated y postition with and existing x/z positions
+        transform.position = new Vector3(transform.position.x, yPos, transform.position.z);
+
     }
 }
