13d7957f6086d2648e73ada3bd23da68f3b0f1d1
AndresGutierrez414 <andresgutierrez414@outlook.com>
2023-04-10 01:48:43 -0500

    made the lavaBallEnvironmental script launch lava balls into the air

--- FPS Shooter/Assets/Scripts/lavaBallEnvironmental.cs
+++ FPS Shooter/Assets/Scripts/lavaBallEnvironmental.cs
@@ -5,12 +5,28 @@ using UnityEngine;
 public class lavaBallEnvironmental : MonoBehaviour
 {
     // variables //
+    [SerializeField] float launchForce;
+    //[SerializeField] float peakHeight;
+    [SerializeField] float timeToLive;
+
+    private Rigidbody rb;
+    //private float initialYPosition;
+    //private bool isDescending = false;
 
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody>();
+
+        // apply force to launch lava ball up //
+        rb.AddForce(Vector3.up * launchForce, ForceMode.Impulse);
+
+        // store initial y position //
+        //initialYPosition = transform.position.y;
+
+        // destroy lava ball after time passed //
+        Destroy(gameObject, timeToLive);
     }
 
     // Update is called once per frame
