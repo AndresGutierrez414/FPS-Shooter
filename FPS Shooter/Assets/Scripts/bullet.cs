f1f278ee592be8dc43d41d6c7a78b33c8f5431c3
AndresGutierrez414 <andresgutierrez414@outlook.com>
2023-04-10 00:53:44 -0500

    added rotation function for lava ball in bullet Update function

--- FPS Shooter/Assets/Scripts/bullet.cs
+++ FPS Shooter/Assets/Scripts/bullet.cs
@@ -7,11 +7,18 @@ public class bullet : MonoBehaviour
     // variables //
     [SerializeField] int damage;
     [SerializeField] int timer;
+    [SerializeField] float rotationSpeed;
 
     // Start is called before the first frame update
     void Start()
     {
         Destroy(gameObject, timer);
+    }
+
+    private void Update()
+    {
+        // rotate projectile //
+        transform.Rotate(rotationSpeed * Time.deltaTime, rotationSpeed * Time.deltaTime, 0);
     }
 
     private void OnTriggerEnter(Collider other)
