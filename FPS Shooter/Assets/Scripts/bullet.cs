6b3ae8c3ea6d4e8f180b853ffc4fc5cb8737343b
AndresGutierrez414 <andresgutierrez414@outlook.com>
2023-04-09 19:58:39 -0500

    added to bullet script

--- FPS Shooter/Assets/Scripts/bullet.cs
+++ FPS Shooter/Assets/Scripts/bullet.cs
@@ -4,15 +4,24 @@ using UnityEngine;
 
 public class bullet : MonoBehaviour
 {
+    // variables //
+    [SerializeField] int damage;
+    [SerializeField] int timer;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        Destroy(gameObject, timer);
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnTriggerEnter(Collider other)
     {
-        
+        IDamage damagable = other.GetComponent<IDamage>();
+        if (damagable != null)
+        {
+            damagable.takeDamage(damage);
+        }
+
+        Destroy(gameObject);
     }
 }
