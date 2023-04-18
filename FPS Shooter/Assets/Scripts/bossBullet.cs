ca182fe6e83132b27016038a9d3477f547d34e03
AndresGutierrez414 <andresgutierrez414@outlook.com>
2023-04-17 19:50:32 -0500

    created new script for boss projectile

---
+++ FPS Shooter/Assets/Scripts/bossBullet.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class bossBullet : MonoBehaviour
+{
+    // variables //
+    [SerializeField] int damage;
+    [SerializeField] int timer;
+    [SerializeField] float rotationSpeed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // destroy projectile after set time //
+        Destroy(gameObject, timer);
+    }
+
+    private void Update()
+    {
+        // rotate projectile //
+        transform.Rotate(rotationSpeed * Time.deltaTime, rotationSpeed * Time.deltaTime, 0);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        IDamage damagable = other.GetComponent<IDamage>();
+        // check if collision is with object that can take damage //
+        if (damagable != null)
+        {
+            damagable.takeDamage(damage);
+
+        }
+
+        Destroy(gameObject);
+    }
+}
