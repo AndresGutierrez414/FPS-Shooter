a19e482859ff558e6bd20487f12826744148a85d
AndresGutierrez414 <andresgutierrez414@outlook.com>
2023-04-18 16:49:30 -0500

    added explosion audios and updated bullet script

--- FPS Shooter/Assets/Scripts/bullet.cs
+++ FPS Shooter/Assets/Scripts/bullet.cs
@@ -5,16 +5,24 @@ using UnityEngine;
 public class bullet : MonoBehaviour
 {
     // variables //
+    [Header("----------Stats----------")]
     [SerializeField] int damage;
     [SerializeField] int timer;
     [SerializeField] float rotationSpeed;
 
+    [Header("----------Components/Prefabs----------")]
     [SerializeField] private GameObject explosionPrefab;
+    [SerializeField] private AudioClip explosionSound;
+
+    private AudioSource audioSource;
+
 
     void Start()
     {
         // destroy projectile after set time //
         Destroy(gameObject, timer);
+
+        audioSource = GetComponent<AudioSource>();
     }
 
     private void Update()
@@ -30,10 +38,25 @@ public class bullet : MonoBehaviour
         if (damagable != null)
         {
             damagable.takeDamage(damage);
+
+            // explosion effect //
             GameObject exlosion = Instantiate(explosionPrefab, transform.position, Quaternion.identity);
+            // explosion audio //
+            playExplosionSound();
         }
+        // explosion effect //
         GameObject exlosionFloor = Instantiate(explosionPrefab, transform.position, Quaternion.identity);
+        // explosion audio //
+        playExplosionSound();
 
         Destroy(gameObject);
+    }
+
+    private void playExplosionSound()
+    {
+        if (audioSource != null && explosionSound != null)
+        {
+            audioSource.PlayOneShot(explosionSound);
+        }
     }
 }
