f66e9ab6119d08c181b7870a7802642a2f4097fd
AndresGutierrez414 <andresgutierrez414@outlook.com>
2023-04-21 17:03:32 -0500

    created bullet object for player and script for the bullet
    script is called playerBullet

---
+++ FPS Shooter/Assets/Scripts/playerBullet.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerBullet : MonoBehaviour
+{
+    // variables //
+    [SerializeField] int damage;
+    [SerializeField] int timer;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Destroy(gameObject, timer);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        IDamage damagable = other.GetComponent<IDamage>();
+        if (damagable != null)
+        {
+            damagable.takeDamage(damage);
+        }
+
+        Destroy(gameObject);
+    }
+}
