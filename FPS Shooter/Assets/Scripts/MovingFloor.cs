9f6b502ac15660a5c5396b70f3d35b8d164120ec
TrayCoats <108248909+TrayCoats@users.noreply.github.com>
2023-04-22 15:23:04 -0500

    made a movingFloor  needs a little work the player dont move with the floor

---
+++ FPS Shooter/Assets/Scripts/MovingFloor.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovingFloor : MonoBehaviour
+{
+    public float leftDistance = 5f; // Distance to move left
+    public float rightDistance = 5f; // Distance to move right
+    public float speed = 5f; // Speed of the platform
+
+    private float currentDistance; // Current distance
+    private bool moveLeft = true; // Flag to determine movement direction
+
+    void Update()
+    {
+        // Move the platform horizontally
+        if (moveLeft)
+        {
+            transform.Translate(Vector3.left * speed * Time.deltaTime);
+            currentDistance += speed * Time.deltaTime;
+        }
+        else
+        {
+            transform.Translate(Vector3.right * speed * Time.deltaTime);
+            currentDistance -= speed * Time.deltaTime;
+        }
+
+        // Check for reaching left or right distance
+        if (currentDistance <= -leftDistance || currentDistance >= rightDistance)
+        {
+            // Reverse platform direction when reaching left or right distance
+            moveLeft = !moveLeft;
+
+            // Pause at the end of each movement
+            StartCoroutine(Pause());
+        }
+    }
+
+    IEnumerator Pause()
+    {
+        yield return new WaitForSeconds(3f);
+        // Reset current distance
+        currentDistance = Mathf.Clamp(currentDistance, -leftDistance, rightDistance);
+    }
+}
