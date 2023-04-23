972b264f801089b79760fe2d7eb6e4ed228439a8
AndresGutierrez414 <andresgutierrez414@outlook.com>
2023-04-23 17:16:10 -0500

    some fine tuning completed

--- FPS Shooter/Assets/Scripts/GameFunctionsScripts/cameraControls.cs
+++ FPS Shooter/Assets/Scripts/GameFunctionsScripts/cameraControls.cs
@@ -35,6 +35,7 @@ public class cameraControls : MonoBehaviour
         Cursor.visible = false;
         Cursor.lockState = CursorLockMode.Locked;
 
+        // if intro is going on //
         if (enableIntroSequence)
         {
             // store original start position of camera //
@@ -80,9 +81,12 @@ public class cameraControls : MonoBehaviour
         // move to end pos and look at it //
         yield return moveAndLookAt(endPosition.position, endPosition.position);
 
+        // iterate through focus points in array //
         foreach (Transform focusPoint in focusPoints)
         {
+            // move to next focus point and look at it //
             yield return moveAndLookAt(focusPoint.position, focusPoint.position);
+            // wait until time before going to next point //
             yield return new WaitForSeconds(waitTimeAtFocusPoints);
         }
 
@@ -99,41 +103,51 @@ public class cameraControls : MonoBehaviour
 
     IEnumerator moveAndLookAt(Vector3 targetPosition, Vector3 lookAtPosition)
     {
-        // Add a small offset to the target position to avoid zero vector issues
+        // Add a small offset to the target position to avoid zero vector error //
         targetPosition += new Vector3(0.0001f, 0.0001f, 0.0001f);
 
-        // Move camera to the starting position of the half-circle movement
+        // calculate start pos of half circle movement //
         Vector3 startCirclePosition = targetPosition + (transform.position - targetPosition).normalized * distanceFromFocusPoint;
+
+        // Move camera to the starting position of the half-circle movement //
         while (Vector3.Distance(transform.position, startCirclePosition) > 0.1f)
         {
+            // move camera to start position //
             transform.position = Vector3.Lerp(transform.position, startCirclePosition, introMoveSpeed * Time.deltaTime);
 
+            // calculate and rotate camera around each point //
             Quaternion targetRotation = Quaternion.LookRotation(lookAtPosition - transform.position);
             transform.rotation = Quaternion.Slerp(transform.rotation, targetRotation, introRotationSpeed * Time.deltaTime);
 
+            // wait for next frame //
             yield return null;
         }
 
-        // Calculate the center of the circle and the radius
+        // Calculate the center of the circle and the radius //
         Vector3 circleCenter = targetPosition;
         float radius = distanceFromFocusPoint;
 
-        // Rotate camera around the focus point in a half-circle
+        // initialize angle for half circle movement //
         float angle = 0;
         float targetAngle = 180f;
 
+        // perform half circle movement //
         while (angle < targetAngle)
         {
+            // Calculate the angle increment based on the half-circle move speed //
             float step = halfCircleMoveSpeed * Time.deltaTime;
             angle += step;
 
+            // Calculate the position on the half-circle path using trigonometry //
             float xPos = circleCenter.x + radius * Mathf.Sin(Mathf.Deg2Rad * angle);
-            float yPos = circleCenter.y; // Assuming the movement is on a horizontal plane, otherwise, adjust accordingly
+            float yPos = circleCenter.y + 5; // adjust height of half circle rotation
             float zPos = circleCenter.z + radius * Mathf.Cos(Mathf.Deg2Rad * angle);
 
+            // update camera pos and rotation //
             transform.position = new Vector3(xPos, yPos, zPos);
             transform.rotation = Quaternion.LookRotation(lookAtPosition - transform.position);
 
+            // wait for next frame //
             yield return null;
         }
     }
