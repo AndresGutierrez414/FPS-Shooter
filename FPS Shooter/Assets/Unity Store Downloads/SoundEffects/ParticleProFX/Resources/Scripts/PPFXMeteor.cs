e362a9913a8297f1be312968cd73f3ecb81b55f4
AndresGutierrez414 <andresgutierrez414@outlook.com>
2023-04-22 19:24:38 -0500

    organized Unity store downloads
    also downloaded mideival bridge asset

---
+++ FPS Shooter/Assets/Unity Store Downloads/SoundEffects/ParticleProFX/Resources/Scripts/PPFXMeteor.cs
@@ -0,0 +1,27 @@
+﻿/*=========================================================
+	PARTICLE PRO FX volume one 
+	PPFXMeteor.cs
+
+	
+	(c) 2014
+=========================================================*/
+
+using UnityEngine;
+using System.Collections;
+
+public class PPFXMeteor : MonoBehaviour {
+	
+	Vector3 groundPos = new Vector3(0,0,0);
+	public Vector3 spawnPosOffset = new Vector3(0,0,0);
+	
+	public float speed = 10f;
+	public GameObject detonationPrefab;
+	
+	public bool destroyOnHit;
+	public bool setRateToNull;
+	
+	float dist = 0f;
+	float radius = 2f;
+	
+	ParticleSystem[] psystems;
+}
