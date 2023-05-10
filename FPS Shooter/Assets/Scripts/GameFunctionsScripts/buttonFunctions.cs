f790620030bc865519ab3dd4a270e7bed7d7f7b9
Dragon-Phoenix-Games-Dev <dragonphoenixgamesdev@gmail.com>
2023-05-10 15:30:44 -0500

    main menu, options, credits scenes

--- FPS Shooter/Assets/Scripts/GameFunctionsScripts/buttonFunctions.cs
+++ FPS Shooter/Assets/Scripts/GameFunctionsScripts/buttonFunctions.cs
@@ -5,28 +5,38 @@ using UnityEngine.SceneManagement;
 
 public class buttonFunctions : MonoBehaviour
 {
+
+    public AudioSource audio;
+
     // variables //
     //gameManager manager;
     public void gameStart()
     {
         SceneManager.LoadScene("MainScene");
+        audio.Play();
     }
 
     public void howToPlay()
     {
         gameManager.instance.mainMenu.SetActive(false);
         gameManager.instance.HTPMenu.SetActive(true);
+        audio.Play();
+
     }
 
     public void back()
     {
         gameManager.instance.mainMenu.SetActive(true);
         gameManager.instance.HTPMenu.SetActive(false);
+        audio.Play();
+
     }
 
     public void resume()
     {
         gameManager.instance.unpauseState();
+        audio.Play();
+
 
         // toggle pause bool //
         gameManager.instance.isPaused = !gameManager.instance.isPaused;
@@ -35,6 +45,8 @@ public class buttonFunctions : MonoBehaviour
     public void restart()
     {
         gameManager.instance.unpauseState();
+        audio.Play();
+
 
         // reload the scene we are in //
         SceneManager.LoadScene(SceneManager.GetActiveScene().name);
@@ -47,6 +59,22 @@ public class buttonFunctions : MonoBehaviour
 
     public void MenuScene()
     {
-        SceneManager.LoadScene("MenuScene");// goes to the Menu Scene
+        SceneManager.LoadScene("MenuScene"); // goes to the Menu Scene
+        audio.Play();
+
+    }
+
+    public void OptionsScene()
+    {
+        SceneManager.LoadScene("OptionsMenu"); // goes to Options Menu
+        audio.Play();
+
+    }
+
+    public void CreditsScene()
+    {
+        SceneManager.LoadScene("CreditsScene"); // goes to Options Menu
+        audio.Play();
+
     }
 }
