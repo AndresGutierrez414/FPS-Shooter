afd94c0d2afb689ff8e894306912018aebbf1bba
Trayvion Coats <108248909+TrayCoats@users.noreply.github.com>
2023-05-10 20:47:22 -0500

    .

--- FPS Shooter/Assets/Scripts/SceneLoading/sceneLoader.cs
+++ FPS Shooter/Assets/Scripts/SceneLoading/sceneLoader.cs
@@ -7,22 +7,54 @@ using UnityEngine.UI;
 public class sceneLoader : MonoBehaviour
 {
 
-    [SerializeField] public GameObject _sceneLoader;
-    [SerializeField] public Image loadingBarRaw;
+    [SerializeField] private GameObject _loadingScreen;
+    [SerializeField] private Image _loadingBar;
 
-    public void SceneLoad(int sceneId)
+    public void LoadScene(int sceneId)
     {
-        StartCoroutine(SceneLoadAsync(sceneId));
+        StartCoroutine(LoadSceneAsync(sceneId));
     }
-    IEnumerator SceneLoadAsync(int sceneId)
+
+    private IEnumerator LoadSceneAsync(int sceneId)
     {
         AsyncOperation operation = SceneManager.LoadSceneAsync(sceneId);
-        _sceneLoader.SetActive(true);
+        _loadingScreen.SetActive(true);
 
         while (!operation.isDone)
         {
             float progress = Mathf.Clamp01(operation.progress / .9f);
-            loadingBarRaw.fillAmount = progress;
+            _loadingBar.fillAmount = progress;
+            yield return null;
+        }
+
+        // Add a delay to show the loading screen for a minimum amount of time
+        yield return new WaitForSeconds(25f);
+
+        _loadingScreen.SetActive(false);
+
+        // Add error handling
+        if (operation.isDone && operation.allowSceneActivation)
+        {
+            // Scene loaded successfully
+            Debug.Log("Scene loaded successfully");
+        }
+        else
+        {
+            // Error occurred while loading the scene
+            Debug.LogError("Error loading scene: " + operation);
+        }
+    }
+
+    private IEnumerator FadeOut(CanvasGroup canvasGroup, float duration)
+    {
+        float currentTime = 0f;
+        float startAlpha = canvasGroup.alpha;
+        float targetAlpha = 0f;
+
+        while (currentTime < duration)
+        {
+            currentTime += Time.deltaTime;
+            canvasGroup.alpha = Mathf.Lerp(startAlpha, targetAlpha, currentTime / duration);
             yield return null;
         }
     }
