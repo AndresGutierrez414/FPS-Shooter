6e1c82320cce3ff183cba553620ee45a06df934b
Trayvion Coats <108248909+TrayCoats@users.noreply.github.com>
2023-05-11 08:21:37 -0500

    .

--- FPS Shooter/Assets/Scripts/SceneLoading/sceneLoader.cs
+++ FPS Shooter/Assets/Scripts/SceneLoading/sceneLoader.cs
@@ -7,6 +7,7 @@ using UnityEngine.UI;
 public class sceneLoader : MonoBehaviour
 {
     [SerializeField] private GameObject loadingScreen;
+    [SerializeField] private GameObject UIScreen;
     [SerializeField] private Image loadingBar;
 
     public void LoadScene(int sceneId)
@@ -17,17 +18,22 @@ public class sceneLoader : MonoBehaviour
     private IEnumerator LoadSceneAsync(int sceneId)
     {
         AsyncOperation operation = SceneManager.LoadSceneAsync(sceneId);
+
         operation.allowSceneActivation = false; // Disable auto activation
+
+        UIScreen.SetActive(false);
+
         loadingScreen.SetActive(true);
 
         while (!operation.isDone)
         {
             float progress = Mathf.Clamp01(operation.progress / .9f);
             loadingBar.fillAmount = progress;
+
             // Check if scene is loaded
             if (operation.progress >= 0.9f)
             {
-                yield return new WaitForSeconds(2f); // Add delay if you want
+                yield return new WaitForSeconds(1f);
 
                 operation.allowSceneActivation = true;
             }
@@ -44,22 +50,8 @@ public class sceneLoader : MonoBehaviour
         }
         else
         {
-            // Error occurred while loading the scene
+            // Error while loadin the scene
             Debug.LogError("Error loading scene: " + operation);
-        }
-    }
-
-    private IEnumerator FadeOut(CanvasGroup canvasGroup, float duration)
-    {
-        float currentTime = 0f;
-        float startAlpha = canvasGroup.alpha;
-        float targetAlpha = 0f;
-
-        while (currentTime < duration)
-        {
-            currentTime += Time.deltaTime;
-            canvasGroup.alpha = Mathf.Lerp(startAlpha, targetAlpha, currentTime / duration);
-            yield return null;
         }
     }
 }
