adb49375d779df8376ba1ff9d9d80006f8a6a4c3
Trayvion Coats <108248909+TrayCoats@users.noreply.github.com>
2023-05-21 19:38:10 -0500

    .

--- FPS Shooter/Assets/Scripts/PlayerScripts/playerController.cs
+++ FPS Shooter/Assets/Scripts/PlayerScripts/playerController.cs
@@ -12,8 +12,8 @@ public class playerController : MonoBehaviour, IDamage
 
 
     [Header("----- Player Stats -----")]                        //Player current hit points and maximum hit points
-    [Range(1, 100)][SerializeField] public int HP;
-    [Range(1, 100)] [SerializeField] public int critHP;
+    [Range(1, 10)][SerializeField] public int HP;
+    [Range(1, 10)] [SerializeField] public int critHP;
     [SerializeField] public int critHPPulseSpeed;
     [SerializeField] public float dmgIndicatorDuration;
     int maxHP;
